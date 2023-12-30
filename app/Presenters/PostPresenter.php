463e0485170552c65a933a132eb6776c7567e152
Zitnik <zitnik.cz@gmail.com>
2023-12-30 14:38:31 +0100

    added post page

---
+++ app/Presenters/PostPresenter.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Presenters;
+
+use Nette;
+use Nette\Application\UI\Form;
+
+
+final class PostPresenter extends Nette\Application\UI\Presenter
+{
+	public function __construct(
+		private Nette\Database\Explorer $database,
+	) {
+	}
+
+	public function renderShow(int $postId): void
+	{
+		$post = $this->database
+			->table('posts')
+			->get($postId);
+		if (!$post) {
+			$this->error('Stránka nebyla nalezena');
+		}
+	
+		$this->template->post = $post;
+	}
+}
